a96433031682e68d413072a80427d9ede4cbd7e6
LathaThavamai <latha.thavamani@gmail.com>
2023-02-15 10:05:19 +0100

    Feb -14 practice added routes

--- basic-server/index.js
+++ basic-server/index.js
@@ -1,71 +1,21 @@
 import express from "express";
+import productRoutes from "./routes/products/products.js";
+import songRoutes from "./routes/songs/songs.js";
+import userRoutes from "./routes/users/users.js";
+import miscRoutes from "./routes/misc/misc.js";
 
 const app = express();
+app.use(express.json())
 
-let fruits = [];
-let songs = [
-  {
-    songName: 'perfect',
-    artist: "Ed Sheeran",
-    rating: 5
-  },
-  {
-    songName: 'Shape of you',
-    artist: "Ed Sheeran",
-    rating: 4
-  }
-]
-app.get('/songs', (req, res) => {
-  let rating = req.query.rating;
-  var filtered = songs.filter(p => p.rating == rating);
-  return res.json(filtered)
-})
-
+app.use("/", productRoutes)
+app.use("/", songRoutes)
+app.use("/", userRoutes)
+app.use("/", miscRoutes)
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-app.get("/my-name", (req, res) => {
-  res.send("Sagar Gavand");
-});
 
-app.get('/area-of-circle/:number', (req, res) => {
-  let area = 3.14 * req.params.number * req.params.number;
-  res.send(`${area.toString()}`);
-});
-
-app.get("/authenticate/:username/:password", (req, res) => {
-  let username = req.params.username;
-  let password = req.params.password;
-  let result = (username == "Admin" && password == "123" ? "welcome" : "invalid");
-  res.send(result);
-});
-
-app.get("/get-all-fruits", (req, res) => {
-  res.send(`${fruits}`);
-});
-
-app.get("/add-fruit/:newFruit", (req, res) => {
-  fruits.push(req.params.newFruit);
-  res.send("added");
-});
-
-app.get("/fruits-starts-with/:start", (req, res) => {
-  var filtered = fruits.filter(p => p.startsWith(req.params.start));
-  res.send(`${filtered}`);
-});
-
-app.get("/fruits-end-with/:end", (req, res) => {
-  var filtered = fruits.filter(p => p.endsWith(req.params.end));
-  res.send(`${filtered}`);
-});
-
-app.get("/remove-fruit/:fruit", (req, res) => {
-  var filtered = fruits.filter(p => (p != req.params.fruit));
-  res.send(`${filtered}`);
-});
 app.listen(3001, () => {
-  console.log("Serer started");
+  console.log("Server started");
 });
-
-
