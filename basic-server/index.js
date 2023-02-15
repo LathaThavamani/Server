a751e4804e43e706abc342ccfb5a70d09e82b731
LathaThavamai <latha.thavamani@gmail.com>
2023-02-15 10:16:57 +0100

    Feb - 15 Practice read and write file

--- basic-server/index.js
+++ basic-server/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import fs from 'fs'
 import productRoutes from "./routes/products/products.js";
 import songRoutes from "./routes/songs/songs.js";
 import userRoutes from "./routes/users/users.js";
@@ -7,13 +8,52 @@ import miscRoutes from "./routes/misc/misc.js";
 const app = express();
 app.use(express.json())
 
-app.use("/", productRoutes)
-app.use("/", songRoutes)
-app.use("/", userRoutes)
-app.use("/", miscRoutes)
+app.use("/",productRoutes)
+app.use("/",songRoutes)
+app.use("/",userRoutes)
+app.use("/",miscRoutes)
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
+
+
+
+
+
+function readfromfile(){
+  let content = fs.readFileSync('./data.txt')
+  return Number(content)
+}
+
+function updateToFile(number){
+  // write to file
+  fs.writeFileSync('./data.txt',number.toString())
+} 
+
+
+
+
+
+
+app.get('/counter',(req,res)=>{
+  
+  let counter = readfromfile();
+  res.send(`${counter}`)
+
+})
+
+app.get('/increment',(req,res)=>{
+  let value = req.query.value;
+
+  let counter = readfromfile();
+
+  counter = counter + Number(value);
+
+  updateToFile(counter) // why is it important ? 
+
+  res.send(`${counter}`)
+})
+
 
 
 app.listen(3001, () => {
--- basic-server/routes/songs/songs.js
+++ basic-server/routes/songs/songs.js
@@ -1,37 +1,53 @@
 import { Router } from "express";
+import { readJsonFromFile, writeJsonToFile } from "../../utilities.js";
 const songRoutes = Router();
 
 let songs = []
 
 
-songRoutes.post('/add-song', (req, res) => {
+// songRoutes.post('/add-song', (req, res) => {
 
+//     let songObj = req.body;
+
+//     songs.push(songObj)
+
+//     res.send("added")
+// })
+
+
+// songRoutes.get('/songs', (req, res) => {
+
+//     if (req.query.rating) {
+//         let rating = req.query.rating;
+
+//         return res.json(songs.filter(x => x.rating == rating))
+//     }
+
+//     if (req.query.ratingMin && req.query.ratingMax) {
+//         return res.json(songs.filter(x => x.rating >= req.query.ratingMin && x.rating <= req.query.ratingMax))
+//     }
+
+//     if (req.query.ratings) {
+//         console.log(req.query.ratings)
+//     }
+
+//     return res.json(songs)
+// })
+
+
+songRoutes.post('/songs', (req, res) => {
     let songObj = req.body;
 
+    let songs = readJsonFromFile('./songs.json')
     songs.push(songObj)
+    writeJsonToFile('./songs.json', songs)
 
     res.send("added")
 })
 
-
 songRoutes.get('/songs', (req, res) => {
-
-    if (req.query.rating) {
-        let rating = req.query.rating;
-
-        return res.json(songs.filter(x => x.rating == rating))
-    }
-
-    if (req.query.ratingMin && req.query.ratingMax) {
-        return res.json(songs.filter(x => x.rating >= req.query.ratingMin && x.rating <= req.query.ratingMax))
-    }
-
-    if (req.query.ratings) {
-        console.log(req.query.ratings)
-    }
-
+    let songs = readJsonFromFile('./songs.json')
     return res.json(songs)
 })
-
 export default songRoutes
 
