cec23202cdb3ac39f6493a65a4e5f906c2845ee2
LathaThavamai <latha.thavamani@gmail.com>
2023-02-16 22:48:10 +0100

    16-Feb-CORS implemented- api call from react app

--- basic-server/index.js
+++ basic-server/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import fs from 'fs'
+import cors from 'cors'
 import productRoutes from "./routes/products/products.js";
 import songRoutes from "./routes/songs/songs.js";
 import userRoutes from "./routes/users/users.js";
@@ -7,11 +8,12 @@ import miscRoutes from "./routes/misc/misc.js";
 
 const app = express();
 app.use(express.json())
+app.use(cors())
 
-app.use("/",productRoutes)
-app.use("/",songRoutes)
-app.use("/",userRoutes)
-app.use("/",miscRoutes)
+app.use("/", productRoutes)
+app.use("/", songRoutes)
+app.use("/", userRoutes)
+app.use("/", miscRoutes)
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
@@ -20,29 +22,29 @@ app.get("/", (req, res) => {
 
 
 
-function readfromfile(){
+function readfromfile() {
   let content = fs.readFileSync('./data.txt')
   return Number(content)
 }
 
-function updateToFile(number){
+function updateToFile(number) {
   // write to file
-  fs.writeFileSync('./data.txt',number.toString())
-} 
+  fs.writeFileSync('./data.txt', number.toString())
+}
 
 
 
 
 
 
-app.get('/counter',(req,res)=>{
-  
+app.get('/counter', (req, res) => {
+
   let counter = readfromfile();
   res.send(`${counter}`)
 
 })
 
-app.get('/increment',(req,res)=>{
+app.get('/increment', (req, res) => {
   let value = req.query.value;
 
   let counter = readfromfile();
--- basic-server/routes/songs/songs.js
+++ basic-server/routes/songs/songs.js
@@ -42,11 +42,17 @@ songRoutes.post('/songs', (req, res) => {
     songs.push(songObj)
     writeJsonToFile('./songs.json', songs)
 
-    res.send("added")
+    res.json("added")
 })
 
 songRoutes.get('/songs', (req, res) => {
     let songs = readJsonFromFile('./songs.json')
+
+    if (req.query.rating) {
+        return res.json(songs.filter(x => x.rating == req.query.rating))
+    }
+
+
     return res.json(songs)
 })
 export default songRoutes
